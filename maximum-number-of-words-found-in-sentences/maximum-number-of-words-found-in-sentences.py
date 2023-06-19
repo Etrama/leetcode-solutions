39ff92854f24073f1da086b00d787999967588f0
Etrama <22076465+Etrama@users.noreply.github.com>
2023-06-18 21:46:44 -0400

    Time: 63 ms (25.12%) | Memory: 16.4 MB (56.26%) - LeetSync

--- maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.py
+++ maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.py
@@ -1,8 +1,3 @@
 class Solution:
     def mostWordsFound(self, sentences: List[str]) -> int:
-        max_sent_length = 0
-        for sentence in sentences:
-            temp = sentence.count(" ") + 1
-            if temp > max_sent_length:
-                max_sent_length = temp
-        return max_sent_length
+        return max(sentence.count(" ") for sentence in sentences) + 1
