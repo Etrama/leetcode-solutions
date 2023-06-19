7bac4646f9b9217f639f559baf1653601d4b3efb
Etrama <22076465+Etrama@users.noreply.github.com>
2023-06-18 21:42:28 -0400

    Time: 50 ms (28.52%) | Memory: 16.3 MB (14.13%) - LeetSync

--- goal-parser-interpretation/goal-parser-interpretation.py
+++ goal-parser-interpretation/goal-parser-interpretation.py
@@ -1,3 +1,13 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        return command.replace("()", "o").replace("(al)", "al")
+        result = ""
+        for i in range(0, len(command)):
+            if command[i] == "G":
+                result += "G"
+            elif command[i] == "(" and command[i+1] == ")":
+                result += "o"
+                i += 2
+            elif command[i] == "(" and command[i+1] == "a":
+                result += "al"
+                i += 4
+        return result
