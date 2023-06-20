454ffa1cce3df32c4bfdf48fb0eb15c5ef97e53e
Etrama <22076465+Etrama@users.noreply.github.com>
2023-06-20 12:40:40 -0400

    Time: 72 ms (43.58%) | Memory: 16.6 MB (12.10%) - LeetSync

--- shuffle-string/shuffle-string.py
+++ shuffle-string/shuffle-string.py
@@ -1,6 +1,7 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
         ref_dict = dict(zip(indices,s))
+        print(ref_dict)
         ref_dict = sorted(ref_dict.items())
         result = ""
         for _, val in ref_dict:
