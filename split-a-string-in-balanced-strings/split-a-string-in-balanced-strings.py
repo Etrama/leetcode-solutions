45902f1f75606a2e96a3c53872ec55054474900b
Etrama <22076465+Etrama@users.noreply.github.com>
2023-06-20 12:30:44 -0400

    Time: 46 ms (57.37%) | Memory: 16.1 MB (84.68%) - LeetSync

--- split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.py
+++ split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.py
@@ -1,15 +1,12 @@
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
         count_result = 0
-        count_r = 0
-        count_l = 0
+        running_count = 0
         for letter in s:
             if letter == "R":
-                count_r += 1
+               running_count += 1
             elif letter == "L":
-                count_l += 1
-            if count_r == count_l and count_r != 0 and count_l != 0:
+                running_count -= 1
+            if running_count == 0:
                 count_result += 1
-                count_r = 0
-                count_l = 0
         return count_result
