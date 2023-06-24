f43c858ad0e7f7b776cf1122ca2ca4970fa7fb7c
Etrama <22076465+Etrama@users.noreply.github.com>
2023-06-24 19:13:15 -0400

    Time: 48 ms (53.33%) | Memory: 16.2 MB (70.97%) - LeetSync

--- maximum-repeating-substring/maximum-repeating-substring.py
+++ maximum-repeating-substring/maximum-repeating-substring.py
@@ -1,12 +1,8 @@
 class Solution:
     def maxRepeating(self, sequence: str, word: str) -> int:
         count = 0
-        flag = True
         og_word = word
-        while flag:
-            if word in sequence:
-                count += 1
-                word += og_word
-            else:
-                flag = False
+        while word in sequence:
+            count += 1
+            word += og_word
         return count
