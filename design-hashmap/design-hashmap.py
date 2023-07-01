9168a098ecdf4a280eda9344505848caefcadfc4
Etrama <22076465+Etrama@users.noreply.github.com>
2023-07-01 13:05:35 -0400

    Time: 2613 ms (8.89%) | Memory: 19.4 MB (81.01%) - LeetSync

---
+++ design-hashmap/design-hashmap.py
@@ -0,0 +1,32 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.map = []
+
+    def put(self, key: int, value: int) -> None:
+        if self.get(key) == -1:
+            self.map.append([key, value])
+        else:
+            for index, val in enumerate(self.map):
+                if val[0] == key:
+                    self.map[index] = [key, value]
+                    
+    def get(self, key: int) -> int:
+        if len(self.map) != 0:
+            for x, y in self.map:
+                if x == key:
+                    return y
+        return -1
+        
+    def remove(self, key: int) -> None:
+        for index, value in enumerate(self.map):
+            if value[0] == key:
+                del self.map[index]
+
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
