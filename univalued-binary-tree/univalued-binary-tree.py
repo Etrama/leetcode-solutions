3e4a433e5a432f2ab5472cfc3f296e7556eedfff
Etrama <22076465+Etrama@users.noreply.github.com>
2023-07-04 14:15:56 -0400

    Time: 48 ms (54.14%) | Memory: 16.3 MB (84.01%) - LeetSync

---
+++ univalued-binary-tree/univalued-binary-tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+        value_set = set()
+        def dfs(node):
+            if node is None:
+                value_set.add(None)
+            else:
+                dfs(node.left)
+                value_set.add(node.val)
+                dfs(node.right)
+        dfs(root)
+        value_set.remove(None)
+        print(value_set)
+        return len(value_set) == 1
