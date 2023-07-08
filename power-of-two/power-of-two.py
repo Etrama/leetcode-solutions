fd1073ac75756d73b24496636ec2eebff8b6cfd4
Etrama <22076465+Etrama@users.noreply.github.com>
2023-07-08 18:36:22 -0400

    Time: 43 ms (85.03%) | Memory: 16.2 MB (65.19%) - LeetSync

---
+++ power-of-two/power-of-two.py
@@ -0,0 +1,8 @@
+import math
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n % 2 == 0:
+            n = n // 2
+        return n == 1
