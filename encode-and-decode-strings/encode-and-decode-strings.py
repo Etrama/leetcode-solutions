fd315050406e3bd6941419d778d39399b6f8389c
Etrama <22076465+Etrama@users.noreply.github.com>
2023-07-13 07:28:12 -0400

    Time: 76 ms (88.21%) | Memory: 16.7 MB (84.44%) - LeetSync

---
+++ encode-and-decode-strings/encode-and-decode-strings.py
@@ -0,0 +1,17 @@
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        return "日".join(strs)
+        
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        return s.split("日")
+        
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
