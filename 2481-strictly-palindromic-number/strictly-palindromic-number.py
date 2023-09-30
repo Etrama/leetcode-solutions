bdff90ac8b5592e4c3d821e186a949835c02bd25
Etrama <22076465+Etrama@users.noreply.github.com>
2023-09-30 15:05:30 -0400

    Time: 43 ms (23.24%) | Memory: 16 MB (99.69%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.py
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.py
@@ -1,14 +1,15 @@
 class Solution:     
     def isStrictlyPalindromic(self, num: int) -> bool:
-        base_limit = num-1
-        for base in range(2,base_limit):
-            # convert number to the base needed
-            # check if palindromic
-            result_array = []
-            while num != 0:
-                result_array.insert(0,str(num%base))
-                num = num / base
-            check = "".join(result_array)=="".join(result_array[::-1])
-            if not check:
-                return False
-        return True
+        # base_limit = num-1
+        # for base in range(2,base_limit):
+        #     # convert number to the base needed
+        #     # check if palindromic
+        #     result_array = []
+        #     while num != 0:
+        #         result_array.insert(0,str(num%base))
+        #         num = num / base
+        #     check = "".join(result_array)=="".join(result_array[::-1])
+        #     if not check:
+        #         return False
+        # return True
+        return False
